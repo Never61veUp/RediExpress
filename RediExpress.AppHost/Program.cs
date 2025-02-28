4028835073b92864216980df3fe6c23b4cbde7ca
Алексей Яблонский <savageseed@yandex.ru>
2025-02-28 11:29:07 +0500

    feat(aspire): added database and host project

--- RediExpress.AppHost/Program.cs
+++ RediExpress.AppHost/Program.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin()
+    .WithDataVolume(isReadOnly: false)
+    .AddDatabase("RediExpressDb");
+
+builder.AddProject<Projects.RediExpress_Host>("host")
+    .WithReference(postgres)
+    .WaitFor(postgres);
+
 builder.Build().Run();
