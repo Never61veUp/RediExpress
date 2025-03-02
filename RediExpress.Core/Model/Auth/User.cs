6021ed8efd108b83f31cbbd522f5acb3657c5314
Алексей Яблонский <savageseed@yandex.ru>
2025-03-02 18:09:25 +0500

    refactor(core): User now inherits from Entity<Guid>

--- RediExpress.Core/Model/Auth/User.cs
+++ RediExpress.Core/Model/Auth/User.cs
@@ -3,14 +3,14 @@ using RediExpress.Core.Model.ValueObjects;
 
 namespace RediExpress.Core.Model.Auth;
 
-public class User : IAggregateRoot
+public class User : Entity<Guid>, IAggregateRoot
 {
     public FullName FullName { get; }
     public Email Email { get; }
     public PhoneNumber PhoneNumber { get; }
     public string PasswordHash { get; }
     
-    private User(FullName fullName, Email email, PhoneNumber phoneNumber, string passwordHash)
+    private User(Guid id, FullName fullName, Email email, PhoneNumber phoneNumber, string passwordHash) : base(id)
     {
         FullName = fullName;
         Email = email;
@@ -20,6 +20,6 @@ public class User : IAggregateRoot
     
     public static Result<User> Create(Guid id, FullName fullName, Email email, PhoneNumber phoneNumber, string passwordHash)
     {
-        return new User(fullName, email, phoneNumber, passwordHash);
+        return new User(id, fullName, email, phoneNumber, passwordHash);
     }
 }
